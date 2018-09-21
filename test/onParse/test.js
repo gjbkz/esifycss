4b6bb4be8e1573261c094fadf2ec637b1adc8e26
Kei Ito <kei.itof@gmail.com>
2018-09-21 16:01:26 +0900

    test onParse

---
+++ test/onParse/test.js
@@ -0,0 +1,51 @@
+const path = require('path');
+const os = require('os');
+const t = require('tap');
+const afs = require('@nlib/afs');
+const postcss = require('postcss');
+const esifycss = require('../../lib');
+const {getExports} = require('../util');
+
+t.test('es', async (t) => {
+    const directory = await afs.mkdtemp(path.join(os.tmpdir(), 'es'));
+    await afs.deploy(directory, {
+        'foo.css': '.foo {--color: red}',
+        'bar': {
+            'bar.css': '.bar {--color: blue}',
+        },
+    });
+    const dest = path.join(directory, 'output.css');
+    const args = [];
+    await esifycss.start({
+        patterns: path.join(directory, '*.css'),
+        base: directory,
+        dest,
+        onParse: async (data) => {
+            args.push(data);
+            await null;
+        },
+    });
+    t.match(args[0], {
+        file: path.join(directory, 'foo.css'),
+        classes: {foo: '_foo_css_foo'},
+        properties: {color: 'red'},
+    });
+    await t.rejects(afs.readFile(path.join(directory, 'bar/bar.css.js')));
+    const outputJS = await afs.readFile(path.join(directory, 'foo.css.js'), 'utf8');
+    const exported = getExports(outputJS);
+    t.match(exported, {
+        classes: {foo: '_foo_css_foo'},
+        properties: {color: 'red'},
+        default: {
+            classes: {name: 'classes'},
+            properties: {name: 'properties'},
+        },
+    });
+    const outputCSS = await afs.readFile(dest, 'utf8');
+    const cssAST = postcss.parse(outputCSS);
+    t.equal(cssAST.nodes[0].selector, `.${exported.classes.foo}`);
+    t.match(cssAST.nodes[0].nodes[0], {
+        prop: '--color',
+        value: exported.properties.color,
+    });
+});
