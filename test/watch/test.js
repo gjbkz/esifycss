854c7177c02a96e40a252d49c6d256d53cd2b74d
Kei Ito <kei.itof@gmail.com>
2018-10-11 13:46:12 +0900

    normalize path

--- test/watch/test.js
+++ test/watch/test.js
@@ -46,21 +46,19 @@ t.test('watch', {timeout: 5000}, (t) => {
         });
         const outputJS = await afs.readFile(path.join(directory, 'foo.css.js'), 'utf8');
         const exported = getExports(outputJS);
-        t.match(exported, {
-            classes: {foo: '_foo_css_foo'},
-            properties: {color: 'gold'},
-            default: {
-                classes: {name: 'classes'},
-                properties: {name: 'properties'},
-            },
-        });
+        t.match(exported.classes, {foo: '_foo_css_foo'}, 'classes');
+        t.match(exported.properties, {color: 'gold'}, 'properties');
+        t.match(exported.default, {
+            classes: {name: 'classes'},
+            properties: {name: 'properties'},
+        }, 'default');
         const outputCSS = await afs.readFile(dest, 'utf8');
         const cssAST = postcss.parse(outputCSS);
-        t.equal(cssAST.nodes[0].selector, `.${exported.classes.foo}`);
+        t.equal(cssAST.nodes[0].selector, `.${exported.classes.foo}`, 'cssAST.nodes[0].selector');
         t.match(cssAST.nodes[0].nodes[0], {
             prop: '--color',
             value: exported.properties.color,
-        });
+        }, 'cssAST.nodes[0].nodes[0]');
     });
     t.end();
 });
