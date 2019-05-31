59a722b02ab607ad85e85f2902cdc31ff32c37db
Kei Ito <kei.itof@gmail.com>
2019-05-31 15:01:55 +0900

    feat: print information on writeFile

--- src/util/fs.ts
+++ src/util/fs.ts
@@ -1,6 +1,8 @@
 import * as fs from 'fs';
 import * as util from 'util';
+import * as stream from 'stream';
 import {createDirectoryFor} from './createDirectoryFor';
+import {write} from './write';
 
 export const readdir = util.promisify(fs.readdir);
 export const readFile = util.promisify(fs.readFile);
@@ -8,15 +10,18 @@ export const stat = util.promisify(fs.stat);
 export const writeFile = (
     dest: string,
     data: string | Buffer,
+    stdout: stream.Writable = process.stdout,
 ): Promise<void> => new Promise((resolve, reject): void => {
     createDirectoryFor(dest);
+    const buffer = Buffer.isBuffer(data) ? data : Buffer.from(data);
     fs.writeFile(
         dest,
-        data,
+        buffer,
         (error) => {
             if (error) {
                 reject(error);
             } else {
+                write(stdout, [`write ${dest} (${buffer.length})`]);
                 resolve();
             }
         },
