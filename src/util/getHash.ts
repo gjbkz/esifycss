37fde07ba4878462d4c71d13d82658aaef94aac1
Kei Ito <kei.itof@gmail.com>
2019-06-21 15:38:19 +0900

    fix: add safe-base64 encoding and make it default

--- src/util/getHash.ts
+++ src/util/getHash.ts
@@ -2,14 +2,17 @@ import {Hash, createHash, HexBase64Latin1Encoding} from 'crypto';
 
 export interface IHashOptions {
     algorithm: string,
-    encoding: HexBase64Latin1Encoding | typeof Buffer,
+    encoding: HexBase64Latin1Encoding | 'safe-base64' | typeof Buffer,
 }
 
 export const getHash = (
     data: Parameters<Hash['update']>[0],
-    {algorithm, encoding}: IHashOptions = {algorithm: 'sha256', encoding: 'base64'},
+    {algorithm, encoding}: IHashOptions = {algorithm: 'sha256', encoding: 'safe-base64'},
 ): string | Buffer => {
     const hash = createHash(algorithm);
     hash.update(data);
+    if (encoding === 'safe-base64') {
+        return hash.digest('base64').split('+').join('-').split('/').join('_').replace(/[=]+$/, '');
+    }
     return encoding === Buffer ? hash.digest() : hash.digest(encoding as HexBase64Latin1Encoding);
 };
