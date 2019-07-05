21fdf2cfef9c84b424c7d1b518c18ef3bb5200d0
Kei Ito <kei.itof@gmail.com>
2019-07-05 12:25:03 +0900

    refactor: split serializeTimingFunction

--- src/parser/serializeTimingFunction.ts
+++ src/parser/serializeTimingFunction.ts
@@ -1,6 +1,36 @@
-import {ICSSTimingFunction, CSSTimingFunctionType, CSSDefinedCubicBezierName, CSSDefinedStepName, CSSStepPosition} from './types';
+import {ICSSTimingFunction, CSSTimingFunctionType, CSSDefinedCubicBezierName, CSSDefinedStepName, CSSStepPosition, ICSSCubicBezierFunction, ICSSStepFunction} from './types';
 import {isSameCubicBezierPoints} from './isSameCubicBezierPoints';
 import {knownCubicBezierPoints} from './constants';
+
+export const serializeCubicBezierTimingFunction = (
+    timingFunction: ICSSCubicBezierFunction,
+): string => [
+    CSSDefinedCubicBezierName.ease,
+    CSSDefinedCubicBezierName.easeIn,
+    CSSDefinedCubicBezierName.easeOut,
+    CSSDefinedCubicBezierName.easeInOut,
+].find((name) => isSameCubicBezierPoints(
+    timingFunction.points,
+    knownCubicBezierPoints[name],
+)) || `cubic-bezier(${timingFunction.points.join(',')})`;
+
+export const serializeStepTimingFunction = (
+    timingFunction: ICSSStepFunction,
+): string => {
+    if (timingFunction.stepCount === 1) {
+        switch (timingFunction.stepPosition) {
+        case CSSStepPosition.start:
+            return CSSDefinedStepName.stepStart;
+        case CSSStepPosition.end:
+            return CSSDefinedStepName.stepEnd;
+        default:
+        }
+    }
+    if (timingFunction.stepPosition === CSSStepPosition.end) {
+        return `steps(${timingFunction.stepCount})`;
+    }
+    return `steps(${timingFunction.stepCount}, ${timingFunction.stepPosition})`;
+};
 
 export const serializeTimingFunction = (
     timingFunction: ICSSTimingFunction,
@@ -9,29 +39,9 @@ export const serializeTimingFunction = (
     case CSSTimingFunctionType.linear:
         return CSSTimingFunctionType.linear;
     case CSSTimingFunctionType.cubicBezier:
-        return [
-            CSSDefinedCubicBezierName.ease,
-            CSSDefinedCubicBezierName.easeIn,
-            CSSDefinedCubicBezierName.easeOut,
-            CSSDefinedCubicBezierName.easeInOut,
-        ].find((name) => isSameCubicBezierPoints(
-            timingFunction.points,
-            knownCubicBezierPoints[name],
-        )) || `cubic-bezier(${timingFunction.points.join(',')})`;
+        return serializeCubicBezierTimingFunction(timingFunction);
     case CSSTimingFunctionType.step:
-        if (timingFunction.stepCount === 1) {
-            switch (timingFunction.stepPosition) {
-            case CSSStepPosition.start:
-                return CSSDefinedStepName.stepStart;
-            case CSSStepPosition.end:
-                return CSSDefinedStepName.stepEnd;
-            default:
-            }
-        }
-        if (timingFunction.stepPosition === CSSStepPosition.end) {
-            return `steps(${timingFunction.stepCount})`;
-        }
-        return `steps(${timingFunction.stepCount}, ${timingFunction.stepPosition})`;
+        return serializeStepTimingFunction(timingFunction);
     default:
         throw new Error(`Invalid timingFunction: ${JSON.stringify(timingFunction)}`);
     }
