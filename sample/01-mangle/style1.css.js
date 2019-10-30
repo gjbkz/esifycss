79318d20ba6e6758f95e8e18ab9ce0e5a85fc32b
Kei Ito <kei.itof@gmail.com>
2019-10-30 12:13:14 +0900

    chore: rebuild sample

---
+++ sample/01-mangle/style1.css.js
@@ -0,0 +1,13 @@
+import {addStyle} from './helper.js';
+addStyle(["WYIGuBCQSCaAQEcSCaAUEE","WYIGwBCQSCeAgBiBIIQkBmBEcSCeAgBiByBkBmBEE","0BGQC2BA4BKOA6BoBIqBIGuBKE","sBGUCOM8BAUoBKOM+BMgCAiCKOMkCMmCAqBKE","sBGuCGwCCOMoCAGwBKE"]);
+export const className = {
+    "icon": "_1",
+    "rotate": "_2"
+};
+export const id = {
+    "container": "_0"
+};
+export const keyframes = {
+    "FadeIn": "_3",
+    "Rotate": "_4"
+};
---
+++ sample/01-mangle/style2.css.js
@@ -0,0 +1,13 @@
+import {addStyle} from './helper.js';
+addStyle(["WYIGqCCQSCaAQEcSCaAUEE","WYIGsCCQSCeAgBiBIIQkBmBEcSCeAgBiByBkBmBEE","0BGyCC2BA4BKOA6BoBIqBIGqCKE","sBG0CCOM8BAUoBKOM+BMgCAiCKOMkCMmCAqBKE","sBG2CG4CCOMoCAGsCKE"]);
+export const className = {
+    "icon": "_6",
+    "rotate": "_7"
+};
+export const id = {
+    "container": "_5"
+};
+export const keyframes = {
+    "FadeIn": "_8",
+    "Rotate": "_9"
+};
