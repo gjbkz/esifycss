495f0347ac58c6480b8371b11851b34852394bcd
Kei Ito <kei.itof@gmail.com>
2020-06-19 09:58:49 +0900

    fix: ignore invalid arguments

--- src/minifier/extractCSSFromArrayExpression.ts
+++ src/minifier/extractCSSFromArrayExpression.ts
@@ -6,30 +6,25 @@ export const extractCSSFromArrayExpression = (
     node: INode,
     cssKey: string,
 ): Array<ICSSRange> => {
-    if (!isArrayExpression(node)) {
-        throw new Error(`InvalidArrayExpression: ${node.type}`);
-    }
     const result: Array<ICSSRange> = [];
-    for (const item of node.elements) {
-        if (!isObjectExpression(item)) {
-            throw new Error(`InvalidObjectExpression: ${item.type}`);
-        }
-        const {properties} = item;
-        if (properties.length !== 1) {
-            throw new Error(`UnexpectedMultipleProperty: ${JSON.stringify(properties, null, 2)}`);
-        }
-        const [{key, value}] = properties;
-        if (key.name !== cssKey) {
-            throw new Error(`InvalidKey: ${JSON.stringify(key)}`);
-        }
-        if (value.type !== 'Literal') {
-            throw new Error(`NonLiteral: ${JSON.stringify(value)}`);
-        }
-        const css = value.value;
-        if (typeof css === 'string') {
-            result.push({css, start: item.start, end: item.end});
-        } else {
-            throw new Error(`InvalidCSSType: ${JSON.stringify(css)}`);
+    if (isArrayExpression(node)) {
+        for (const item of node.elements) {
+            if (isObjectExpression(item)) {
+                for (const {key, value} of item.properties) {
+                    if (key.name === cssKey) {
+                        if (value.type === 'Literal') {
+                            const css = value.value;
+                            if (typeof css === 'string') {
+                                result.push({css, start: item.start, end: item.end});
+                            } else {
+                                throw new Error(`InvalidCSSType: ${JSON.stringify(css)}`);
+                            }
+                        } else {
+                            throw new Error(`NonLiteral: ${JSON.stringify(value)}`);
+                        }
+                    }
+                }
+            }
         }
     }
     return result;
