3cd6b5840def3da6b0e5df925c298c08d724707b
Kei Ito <kei.itof@gmail.com>
2020-07-11 16:16:30 +0900

    build: build for each environments

--- ava.config.all.cjs
+++ ava.config.all.cjs
@@ -1,32 +1,3 @@
-const fs = require('fs');
-const path = require('path');
-/**
- * @param directory {string}
- * @returns {Array<string>}
- */
-const findTestFile = (directory) => {
-    const result = [];
-    for (const name of fs.readdirSync(directory)) {
-        const filePath = path.join(directory, name);
-        const stats = fs.statSync(filePath);
-        if (stats.isDirectory()) {
-            result.push(...findTestFile(filePath));
-        } else if (name.endsWith('.test.js')) {
-            result.push(filePath);
-        }
-    }
-    return result;
-};
-
-/**
- * @param context {{projectDir: string}}
- */
-module.exports = ({projectDir}) => {
-    console.log({projectDir});
-    const files = findTestFile(path.join(projectDir, 'lib'));
-    console.log(files);
-    return {
-        files: files.map((filePath) => path.relative(projectDir, filePath)),
-        extensions: ['js'],
-    };
+module.exports = {
+    extensions: ['js'],
 };
