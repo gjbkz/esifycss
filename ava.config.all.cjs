d358ced00049f066d84893c9c1bbb19ad2d57c7c
Kei Ito <kei.itof@gmail.com>
2020-07-11 15:43:55 +0900

    chore: use native api to list test files

--- ava.config.all.cjs
+++ ava.config.all.cjs
@@ -1,7 +1,32 @@
-const fg = require('fast-glob');
-const files = fg.sync('lib/**/*.test.js');
-console.log(files);
-module.exports = {
-    files,
-    extensions: ['js'],
+const fs = require('fs');
+const path = require('path');
+/**
+ * @param directory {string}
+ * @returns {Array<string>}
+ */
+const findTestFile = (directory) => {
+    const result = [];
+    for (const name of fs.readdirSync(directory)) {
+        const filePath = path.join(directory, name);
+        const stats = fs.statSync(filePath);
+        if (stats.isDirectory()) {
+            result.push(...findTestFile(filePath));
+        } else if (name.endsWith('.test.js')) {
+            result.push(filePath);
+        }
+    }
+    return result;
+};
+
+/**
+ * @param context {{projectDir: string}}
+ */
+module.exports = ({projectDir}) => {
+    console.log({projectDir});
+    const files = findTestFile(path.join(projectDir, 'lib'));
+    console.log(files);
+    return {
+        files: files.map((filePath) => path.relative(projectDir, filePath)),
+        extensions: ['js'],
+    };
 };
