af229ebe5d2772ee9f733d0b1f18163f12e10331
Kei Ito <kei.itof@gmail.com>
2020-07-13 01:30:55 +0900

    fix: support <url>

--- src/postcssPlugin/parseImport.ts
+++ src/postcssPlugin/parseImport.ts
@@ -5,14 +5,17 @@ export const parseImport = (
     parameter: string,
     id: string,
 ): null | {from: string, localName: string} => {
-    const [importFrom, localName, ...others] = parameter.split(/\s+/);
-    if (others.length === 0 && (/^(['"]).+\1/).exec(importFrom) && (/^[\w-]+$/).exec(localName)) {
-        const from = importFrom.slice(1, -1);
-        if (from.startsWith('.')) {
-            return {
-                from: normalizePath(path.join(path.dirname(id), ...from.split(/\//))),
-                localName,
-            };
+    const matched = (/^(?:url\(\s*)?(['"])(.+)\1\s*\)?\s*/).exec(parameter);
+    if (matched) {
+        const localName = parameter.slice(matched[0].length);
+        if ((/^[\w-]+$/).exec(localName)) {
+            const from = matched[2];
+            if (from.startsWith('.')) {
+                return {
+                    from: normalizePath(path.join(path.dirname(id), ...from.split(/\//))),
+                    localName,
+                };
+            }
         }
     }
     return null;
