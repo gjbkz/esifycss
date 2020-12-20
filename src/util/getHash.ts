def3b76edc233383b2dea0efebec15aee58bf2fb
Kei Ito <kei.itof@gmail.com>
2020-12-20 15:12:39 +0900

    fix: a type error

--- src/util/getHash.ts
+++ src/util/getHash.ts
@@ -1,18 +1,22 @@
-import {Hash, createHash, HexBase64Latin1Encoding} from 'crypto';
+import {Hash, createHash, BinaryToTextEncoding} from 'crypto';
 
 export interface IHashOptions {
     algorithm: string,
-    encoding: HexBase64Latin1Encoding | 'safe-base64' | typeof Buffer,
+    encoding: BinaryToTextEncoding | 'base64url' | 'buffer',
 }
 
 export const getHash = (
     data: Parameters<Hash['update']>[0],
-    {algorithm, encoding}: IHashOptions = {algorithm: 'sha256', encoding: 'safe-base64'},
+    {algorithm, encoding}: IHashOptions = {algorithm: 'sha256', encoding: 'base64url'},
 ): string | Buffer => {
     const hash = createHash(algorithm);
     hash.update(data);
-    if (encoding === 'safe-base64') {
+    switch (encoding) {
+    case 'base64url':
         return hash.digest('base64').split('+').join('-').split('/').join('_').replace(/[=]+$/, '');
+    case 'buffer':
+        return hash.digest();
+    default:
+        return hash.digest(encoding);
     }
-    return encoding === Buffer ? hash.digest() : hash.digest(encoding as HexBase64Latin1Encoding);
 };
