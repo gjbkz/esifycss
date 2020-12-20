5508c4b8bcc0757f0dbf802babc4961b21cbc494
Kei Ito <kei.itof@gmail.com>
2020-12-20 15:49:17 +0900

    chore: rebuild

--- sample/02-no-mangle/style1.css.js
+++ sample/02-no-mangle/style1.css.js
@@ -1,5 +1,5 @@
 import {addStyle} from './helper.js';
-addStyle(["YaIcGyCCOQCeAOEgBQCeAWEE","YaIiBG0CCOQCkBASmBIIOoBqBEgBQCkBASmB4BoBqBEE","6B8BGOC+BAgCUMAiCsBIuBIcGyCE","wByBGWCMKkCAWsBUMKmCKoCAqCUMKsCKuCAuBE","wByBG+CSGgDCMKwCAiBG0CE"]);
+addStyle(["YaIcG0BCOQCeAOEgBQCeAWEE","YaIiBG2BCOQCkBASmBIIOoBqBEgBQCkBASmB4BoBqBEE","6B8BGOC+BAgCUMAiCsBIuBIcG0BE","wByBGWCMKkCAWsBUMKmCKoCAqCUMKsCKuCAuBE","wByBG2CSG4CCMKwCAiBG2BE"]);
 export const className = {
     "icon": "icon_1",
     "rotate": "rotate_2"
