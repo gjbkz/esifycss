53444a455adb93b0343ba6dc3a1f64fc1f82b5a6
Kei Ito <kei.itof@gmail.com>
2021-03-14 15:29:46 +0900

    chore: rebuild

---
+++ sample/00-src/style2.css.js
@@ -0,0 +1,13 @@
+import {addStyle} from '../01-mangle/helper.js';
+addStyle([{$$esifycss: "@keyframes _q{0%{opacity:0}100%{opacity:1}}"},{$$esifycss: "@keyframes _r{0%{transform:rotate(  0deg)}100%{transform:rotate(360deg)}}"},{$$esifycss: "#_n{display:flex;animation:.2s linear _q}"},{$$esifycss: "._o{animation-duration:1s;animation-iteration-count:infinite;animation-timing-function:linear}"},{$$esifycss: "._o._p{animation-name:_r}"}]);
+export const className = {
+    "icon": "_o",
+    "rotate": "_p"
+};
+export const id = {
+    "container": "_n"
+};
+export const keyframes = {
+    "FadeIn": "_q",
+    "Rotate": "_r"
+};
