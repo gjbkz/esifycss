93dccfa824733c1c3740a0a76f2a9530e70c852a
Kei Ito <kei.itof@gmail.com>
2021-09-11 05:30:34 +0900

    chore: update test configurations

--- ava.config.cjs
+++ ava.config.cjs
@@ -1,6 +1,5 @@
 module.exports = {
     extensions: ['ts'],
     require: ['ts-node/register'],
-    verbose: true,
-    timeout: '1m',
+    timeout: '2m',
 };
--- ava.config.client.cjs
+++
@@ -1,7 +0,0 @@
-const path = require('path');
-module.exports = {
-    files: [path.join('test-client', 'run.ts')],
-    extensions: ['ts'],
-    require: ['ts-node/register'],
-    timeout: '2m',
-};
