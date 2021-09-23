96128331a236c805851e62d6176ef3051e5d3183
Kei Ito <kei.itof@gmail.com>
2021-09-24 00:33:26 +0900

    fix: cache the whole result

--- src/minifier/minifyScriptsForCSS.ts
+++ src/minifier/minifyScriptsForCSS.ts
@@ -2,20 +2,27 @@ import {updateFile} from '../util/updateFile';
 import {parseScripts} from './parseScripts';
 import type {ScriptData} from './types';
 
+const cache = new WeakMap<ScriptData, string>();
+
 export const minifyScriptForCSS = async (
     [file, data]: [string, ScriptData],
 ) => {
-    const cssList: Array<string> = [];
-    let code = data.script;
-    for (let index = data.ranges.length; index--;) {
-        cssList[index] = data.ranges[index].css;
+    let cached = cache.get(data);
+    if (!cached) {
+        const cssList: Array<string> = [];
+        let code = data.script;
+        for (let index = data.ranges.length; index--;) {
+            cssList[index] = data.ranges[index].css;
+        }
+        code = [...data.expressionStatements, ...data.importDeclarations]
+        .sort((range1, range2) => range1.start < range2.start ? 1 : -1)
+        .reduce((node, range) => `${node.slice(0, range.start)}${node.slice(range.end)}`, code)
+        .replace(/\n\s*\n/g, '\n');
+        await updateFile(file, code);
+        cached = cssList.join('\n');
+        cache.set(data, cached);
     }
-    code = [...data.expressionStatements, ...data.importDeclarations]
-    .sort((range1, range2) => range1.start < range2.start ? 1 : -1)
-    .reduce((node, range) => `${node.slice(0, range.start)}${node.slice(range.end)}`, code)
-    .replace(/\n\s*\n/g, '\n');
-    await updateFile(file, code);
-    return cssList.join('\n');
+    return cached;
 };
 
 /**
